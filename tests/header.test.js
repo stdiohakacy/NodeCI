0b6770c524d06625abf543d7d6f9789878a19807
Duy <huynhduytam2210@gmail.com>
2020-08-13 21:51:14 +0700

    Generating Sessions and Signatures

--- tests/header.test.js
+++ tests/header.test.js
@@ -22,3 +22,18 @@ test('Clicking login starts oauth flow', async () => {
     const url = await page.url()
     expect(url).toMatch(/accounts\.google\.com/)
 })
+
+test('When signed in, shows logout button', async () => {
+    const id = ''
+    const Buffer = require('safe-buffer').Buffer
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    }
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
+    const Keygrip = require('keygrip')
+    const keys = require('../config/keys')
+    const keygrip = new Keygrip([keys.cookieKey])
+    const sig = keygrip.sign('session=' + sessionString)
+})
