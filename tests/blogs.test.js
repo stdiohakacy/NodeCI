1958f2d931684298be3db43d760ba3c06fec94a5
Duy <huynhduytam2210@gmail.com>
2020-08-14 01:56:59 +0700

    Asserting Validation Errors

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -20,4 +20,18 @@ describe('When logged in', async () => {
         const label = await page.getContentsOf('form label')
         expect(label).toEqual('Blog Title')
     })
+
+    describe('And using invalid inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button')
+        })
+
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text')
+            const contentError = await page.getContentsOf('.content .red-text')
+
+            expect(titleError).toEqual('You must provide a value')
+            expect(contentError).toEqual('You must provide a value')
+        })
+    })
 })
