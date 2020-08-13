d458313a6c3ea9dfb7712cb2fa2de491eb4cb974
Duy <huynhduytam2210@gmail.com>
2020-08-13 22:33:19 +0700

    WaitFor Statements

--- tests/header.test.js
+++ tests/header.test.js
@@ -9,7 +9,7 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
-    await browser.close()
+    // await browser.close()
 })
 
 test('The header has the correct text', async () => {
@@ -23,17 +23,26 @@ test('Clicking login starts oauth flow', async () => {
     expect(url).toMatch(/accounts\.google\.com/)
 })
 
-test('When signed in, shows logout button', async () => {
-    const id = ''
+test.only('When signed in, shows logout button', async () => {
+    const id = '5f34aabd2c6501290c935f26'
     const Buffer = require('safe-buffer').Buffer
     const sessionObject = {
         passport: {
             user: id
         }
     }
+
     const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
     const Keygrip = require('keygrip')
     const keys = require('../config/keys')
     const keygrip = new Keygrip([keys.cookieKey])
     const sig = keygrip.sign('session=' + sessionString)
+
+    await page.setCookie({name: 'session', value: sessionString})
+    await page.setCookie({name: 'session.sig', value: sig})
+    await page.goto('localhost:3000')
+    await page.waitFor('a[href="/auth/logout"]')
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+    expect(text).toEqual('Logout')
 })
