cc22f53b1ffb161925680de9615ac770f4531439
Duy <huynhduytam2210@gmail.com>
2020-08-13 20:05:39 +0700

    Asserting URL Domain

---
+++ tests/header.test.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer')
+
+let browser, page
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({ headless: false })
+    page = await browser.newPage()
+    await page.goto('localhost:3000')
+})
+
+afterEach(async () => {
+    await browser.close()
+})
+
+test('The header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML)
+    expect(text).toEqual('Blogster')
+})
+
+test('Clicking login starts oauth flow', async () => {
+    await page.click('.right a')
+    const url = await page.url()
+    expect(url).toMatch(/accounts\.google\.com/)
+})
